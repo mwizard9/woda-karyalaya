c80590c0752fad260f5f690368afe42595e540a8
mwizard9 <manishhimalaya64@gmail.com>
2022-08-01 19:12:12 +0545

    frontpage setting

--- wodakaryalaya/views.py
+++ wodakaryalaya/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from requests import request
 
 # Create your views here.
+def index(request):
+    
+    return render(request, 'Index.html')
