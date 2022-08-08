caf03ba90e6cd3e2ada5ac78d67dd486888bc077
mwizard9 <manishhimalaya64@gmail.com>
2022-08-08 14:11:24 +0545

    maintaining model

--- wodakaryalaya/models.py
+++ wodakaryalaya/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Birth(models.Model):
     genders=(
-        ('M','Male'),
-        ('F','Female'),
-        ('O','Others')
+        ('Male','Male'),
+        ('Female','Female'),
+        ('Others','Others')
     )
     fname=models.CharField(max_length=100)
     mname=models.CharField(max_length=100)
