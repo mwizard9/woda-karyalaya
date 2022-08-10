24677a5cceaa7359c03a465b97c659f3ef6d7d44
mwizard9 <manishhimalaya64@gmail.com>
2022-08-10 15:07:42 +0545

    mrc

--- wodakaryalaya/views.py
+++ wodakaryalaya/views.py
@@ -33,8 +33,26 @@ def birth(request):
             return render(request, 'birth.html')
 
 def marriage(request):
-    
-    return render(request, 'marriage.html')
+        if request.method == 'POST':
+            fname=request.POST['fname']
+            mname=request.POST['mname']
+            lname=request.POST['lname']
+            Birthday=request.POST['birthday']
+            fathername=request.POST['fathername']
+            mothername=request.POST['mothername']
+            marriageId=request.POST['mcer']
+            img=request.POST['mimg']
+            provience=request.POST['pname']
+            district=request.POST['dname']
+            city=request.POST['vname']
+            gender=request.POST['gender']
+
+            birth = Birth(fname=fname ,mname=mname ,lname=lname, birthday=Birthday,fathername=fathername,mothername=mothername,marriageId=marriageId,img=img,provience=provience,district=district,city=city,gender=gender)
+            birth.save();
+            print('user created')
+            return redirect(birthc)
+        else:
+            return render(request, 'marriage.html')
 
 def birthc(request):
 
